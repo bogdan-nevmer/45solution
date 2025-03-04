ff4e235f7937b45d5b167f635ddb51b053c6900c
bogdan nevmer <weckman689@gmail.com>
2025-03-04 17:42:17 +0300

    check local min

--- 1/logic.cpp
+++ 1/logic.cpp
@@ -3,9 +3,12 @@
 
 bool check_local_min(int* array, int size){
 	bool local_min = false;
-	for (int i = 0; i < size - 1; i++)
+	if (size > 1 && (array[0] < array[1] || array[size - 1] < array[size - 2]));
+
+	for (int i = 1; i < size - 1; i++)
 	{
-		if (array[i] > array[i + 1] && array[i] > array[i - 1]) {
+		if (array[i] < array[i - 1] &&
+			array[i] < array[i + 1] ) {
 			local_min = true;
 		}
 	}
@@ -16,7 +19,7 @@ bool check_local_min(int* array, int size){
 
 
 bool check_local_max(int* array, int size){
-
+	if (size > 1 && (array[0] < array[1] || array[size - 1] > array[size - 2]));
 	bool local_max = false;
 	for (int i = 0; i < size - 1; i++)
 	{
--- 1/main.cpp
+++ 1/main.cpp
@@ -14,5 +14,9 @@ int main() {
 	init(pointer, size, -20, 20);
 	cout << "array " <<convert(pointer,size) <<endl;
 
+	cout << "does local min " <<(check_local_min(pointer,size)?"yes":"no") << endl;
+
+	cout << "does local max " << (check_local_max(pointer, size) ? "yes" : "no") << endl;
+
 	return 0;
 }
